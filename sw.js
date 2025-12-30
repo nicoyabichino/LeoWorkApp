81c8a083c77ac3178d72a67e36ba3d014f165717
Diego Nicolas Yabichino <89562667+nicoyabichino@users.noreply.github.com>
2025-12-30 13:31:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
-const CACHE_NAME = 'workout-v1';
+const CACHE_NAME = 'leo-work-v2';
 
-self.addEventListener('install', e => {
-  console.log('Service Worker instalado');
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./index.html', './manifest.json']);
+    })
+  );
+  self.skipWaiting(); // Obliga a la versión nueva a activarse
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
+
